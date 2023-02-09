8b3116a9ab44d21d322180ec97b28bd83ab9bcbd
Gervais Jessy <gervaisjessy.mihindoumamboundou@eidia.ueuromed.org>
2023-02-09 03:34:15 +0100

    second commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('salut cest jessy' )
+    print_hi('salut cest jessy comment tu vas ?' )
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
